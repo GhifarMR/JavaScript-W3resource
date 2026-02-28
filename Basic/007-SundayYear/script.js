c39566407f4e9050fbd7e65f3010c012a38e27f1
Ghifar Maulana Ristyono <tyrtwqptllpck@gmail.com>
2026-03-01 06:52:42 +0700

    add 007-SundayYear solution files

---
+++ Basic/007-SundayYear/script.js
@@ -0,0 +1,9 @@
+
+for(let year = 2014; year <= 2050; year++) {
+    const date = new Date(year, 0, 1);
+
+    if(date.getDay() === 0) {
+        console.log(`1st January is being a Sunday ${year}`);
+    }
+}
+
