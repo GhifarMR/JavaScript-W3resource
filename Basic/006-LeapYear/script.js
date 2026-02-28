e97c4f7c27f6eb3dd5d36347f5bd408fb293a633
Ghifar Maulana Ristyono <tyrtwqptllpck@gmail.com>
2026-03-01 06:36:51 +0700

    add 006-LeapYear files

---
+++ Basic/006-LeapYear/script.js
@@ -0,0 +1,12 @@
+const isLapYear = (year) => {
+  const check = year % 100 === 0 ? year % 400 === 0 : year % 4 === 0;
+  check ? "this is lap year" : "this is not lap year";
+
+  console.log(check);
+};
+
+isLapYear(2003);
+isLapYear(2016);
+isLapYear(2000);
+isLapYear(2040);
+isLapYear(1945);
