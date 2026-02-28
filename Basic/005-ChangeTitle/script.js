b6c706b2dace11a62ebcdf8e81d76ccbc77117d1
Ghifar Maulana Ristyono <tyrtwqptllpck@gmail.com>
2026-03-01 06:27:45 +0700

    add 005-ChangeTitle/script.js

--- Basic/005-ChangeTitle/script.js
+++ Basic/005-ChangeTitle/script.js
@@ -2,19 +2,13 @@ const animateString = (id) => {
   const element = document.getElementById(id);
 
   const textNode = element.childNodes[0];
-  console.log(textNode);
-
   let text = textNode.data;
-  console.log(text);
-
-  console.log(text.slice(-1));
-  console.log(text.slice(0, -1));
-  console.log(text.slice(-1) + text.slice(0, -1));
+  
   setInterval(() => {
     text = text.slice(-1) + text.slice(0, -1);
-
-    textNode.data = text;
+    element.innerHTML = text
   }, 100);
-};
+
+}
 
 animateString("textTitle");
